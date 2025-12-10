7d0b25a8d42ad08becd2d5a1a6d6e30bfcd31edf
SlepoyShaman <ilushin905@gmail.com>
2025-12-10 23:13:05 +0300

    123123

--- packages/markitdown/src/markitdown/__about__.py
+++ packages/markitdown/src/markitdown/__about__.py
@@ -1,4 +1,3 @@
-# SPDX-FileCopyrightText: 2024-present Adam Fourney <adamfo@microsoft.com>
-# asdasdwqesadqweqweqweqwe23131231213131231231
+#jkfhfhjkrghkjlrgjkhlrghjklgrqweqwe23131231213131231231
 # aboba aboba312
 __version__ = "0.1.4"
